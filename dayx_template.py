600decbca0b71723944d21e5de82a4eacbe28af8
David Dan-Du Nguyen <dn103214@fh-muenster.de>
2022-12-14 19:54:14 +0100

    add clang tidy to template

--- dayx_template.py
+++ dayx_template.py
@@ -1,18 +1,24 @@
+# constants
+DEBUG = True
+DAY = 0
+
+# open file
+if DEBUG:
+    file = open("input" + str(DAY) + "_mock.txt")
+else:
+    file = open("input" + str(DAY) + ".txt")
+text = file.readlines()
+line_count = text.__len__()
+
+# var
+
+
 if __name__ == '__main__':
     
-    # constants
-    DEBUG = True
-    DAY = 0
-    
-    # open file
-    if DEBUG:
-        file = open("input" + str(DAY) + "_mock.txt")
-    else:
-        file = open("input" + str(DAY) + ".txt")
-    text = file.readlines()
-    line_count = text.__len__()
-    
     # main program here
+    
+    # print solution
+    print(sol)
     
     # end of program reached
     print("end of program reached")
